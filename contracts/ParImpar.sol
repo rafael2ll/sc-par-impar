9832028c1de128eb0ee11564c737f81fb0541108
"Rafael Valentim" <"rafaelvalentim36@gmail.com">
2021-10-01 09:06:36 -0300

    Contract

--- contracts/ParImpar.sol
+++ contracts/ParImpar.sol
@@ -13,7 +13,7 @@ using SimpleCommit for SimpleCommit.CommitType;
 
 // Estados: aguardaJogador, revelaValores12, revelado, Fim....
 
- 
+enum State{START, WAITING_PLAYER, READY_TO_DISCLOSE, OWNER_VALUE_DISCLOSED, PLAYER_VALUE_DISCLOSED, VALUES_DISCLOSED, END}
 
 SimpleCommit.CommitType valorDono;
 SimpleCommit.CommitType valorJogador;
@@ -29,9 +29,12 @@ uint256 valorAposta;
 uint256 limiteBloco;
 
  
+State state = State.START;
 
 // flags!!!!!
 
+bool ownerDisclosed = false;
+bool playerDisclosed = false;
 
 function setValorAposta() internal{
     valorAposta = msg.value;
@@ -43,37 +46,98 @@ constructor (bytes32 _vD) public {
     setValorAposta();
     limiteBloco = block.number + 10; //????
     dono =msg.sender;
-    // armazena estado
+    state = State.WAITING_PLAYER;
 }
 
  
 
+function requireLessValue() internal{
+    require ( msg.value >= valorAposta,"Valor maior do que o da aposta");
+}
 
-modifier somenteDono {require (msg.sender == dono,"Somento o dono pode chamar esta funcao!");_;}
-modifier capValue {require ( msg.value >= valorAposta,"fala alguma coisa");_;}
 
-function entraJogo(bytes32 _vJ) public {
-//   require estado....
+modifier onlyOwner {require (msg.sender == dono,"Somento o dono pode chamar esta funcao!");_;}
+modifier onlyPlayer {require (msg.sender == jogador,"Somento o jogador pode chamar esta funcao!");_;}
+modifier capValue {requireLessValue();_;}
+
+function entraJogo(bytes32 _vJ) public capValue {
+    require(state == State.WAITING_PLAYER, "O jogo já possui um jogador");
     
     valorJogador.commit(_vJ);
-  // atualiza estado do contrato  
+    jogador = msg.sender;
+    
+    state = State.READY_TO_DISCLOSE;
 }
 
  
 
-function donoRevela(bytes32 nonce1, byte _v1) public  {
-  // require dono
-//   require estado...
+function ownerReveal(bytes32 nonce1, byte _v1) public onlyOwner {
+  require(state == State.READY_TO_DISCLOSE || (state == State.PLAYER_VALUE_DISCLOSED && !ownerDisclosed), "Seu valor não pode ser revelado");
+  
   valorDono.reveal(nonce1,_v1);
   //ok = sc1.isCorrect();
+  state = state == State.READY_TO_DISCLOSE ? State.OWNER_VALUE_DISCLOSED : State.VALUES_DISCLOSED;
 }
 
  
 
-// function jogadorRevela(....) public {.....}
+function playerReveal(bytes32 nonce1, byte _v1) public onlyPlayer {
+  require(state == State.READY_TO_DISCLOSE || (state == State.OWNER_VALUE_DISCLOSED && !playerDisclosed), "Seu valor não pode ser revelado");
+  
+  valorJogador.reveal(nonce1,_v1);
+  //ok = sc1.isCorrect();
+  state = state == State.READY_TO_DISCLOSE ? State.PLAYER_VALUE_DISCLOSED : State.VALUES_DISCLOSED;
+}
 
  
+ 
+ function payOwner() internal pure{
+     require(false, "Dono foi o vencedor");
+ }
+ 
+ function payPlayer() internal pure{
+     require(false, "Jogador foi o vencedor");
+ }
+ 
+ function repay() internal pure{
+     require(false, "Dinheiro devolvido");
+     
+ }
+ 
+function handleLimit() private view{
+    if(state == State.VALUES_DISCLOSED){
+        int8 bit = int8(valorDono.value ^ valorJogador.value) % 2 ** 1;
+        if(bytes1(bit) == 0){
+            payOwner();
+        }else{
+            payPlayer();
+        }
+    }else{
+        if(state == State.PLAYER_VALUE_DISCLOSED)
+            payPlayer();
+        else if(state == State.OWNER_VALUE_DISCLOSED)
+            payOwner();
+        else
+            repay();
+    }    
+}
 
+function payWinner() public {
+   if(block.number > limiteBloco)
+        handleLimit();
+    else if(state == State.VALUES_DISCLOSED){
+        int8 bit = int8(valorDono.value ^ valorJogador.value) % 2 ** 1;
+        if(bytes1(bit) == 0){
+            payOwner();
+        }else{
+            payPlayer();
+        }
+    }else{
+        require(false, "O jogo não pode ser finalizado");
+    }
+    state = State.END;
+    
+}
 
 // function pagaVencedor() public {
 // Quem é o vencedor???
